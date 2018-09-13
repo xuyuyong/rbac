72ba733b5bdbe2b58b1b1b43e2692ee4443b913a
xuyuyong <3176229153@qq.com>
2018-09-14 07:22:55 +0800

    测试提交

--- com.rbac.web/src/main/java/com/rbac/controller/UserAction.java
+++ com.rbac.web/src/main/java/com/rbac/controller/UserAction.java
@@ -1,11 +1,16 @@
 package com.rbac.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author xuyuyong
  * Date: 2018/9/12
  * Time: 10:08
  * Content:
  */
+@Controller
+@RequestMapping("/user")
 public class UserAction {
 
 }
